9dc29eb069aa92f901172a80895dd297fcadd822
SuragNuthulapaty <77648585+SuragNuthulapaty@users.noreply.github.com>
2023-09-21 13:33:23 -0500

    Update HomePage.js

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -23,11 +23,9 @@ export function AgePage() {
       className='flex d-flex flex-column w-100 align-items-center justify-content-center fill'
       style={{height:'882px'}}
     >
-      {/* <Navbar/> */}
       <h1 className="display-1 text-dark">Please Enter Your Age</h1>
       <div className="flex d-flex flex-row p-2 align-items-center justify-content-center" style={{height:'100px'}}>
         <input name="age-in" id="input-text" className="m-3 p-2 rounded" value={inputText} onChange={handleChange}/>
-        {/* <p className="mt-1 ml-1">this is some more text</p> */}
         <Link to={{ pathname:"/profile", state:{age:getAge()} }} className="text-white rounded p-2 bg-dark" style={{textDecoration:'none'}}>Submit</Link>
       </div>
     </div>
