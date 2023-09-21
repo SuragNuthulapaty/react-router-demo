be75c0e9cb54ef646dd93c51cdc3a9bf6ff9c325
SuragNuthulapaty <77648585+SuragNuthulapaty@users.noreply.github.com>
2023-09-20 20:25:05 -0500

    start food scarcity shit

--- src/components/FeedPage.js
+++ src/components/FeedPage.js
@@ -1,36 +1,26 @@
 import React from "react";
+import { useState } from "react";
+import { FoodScarcity } from "./FoodScarcity";
 
 export function FeedPage() {
-
-  var page_val = 0
+  const [page_val, set_page_val] = useState(0);
 
   function foodScarcityBtn() {
-    console.log("food scaricity")
-    page_val = 0
+    set_page_val(0)
   }
 
   function secondFocus() {
-    console.log(2)
-    page_val = 1
+    set_page_val(1)
   }
 
   function thirdFocus() {
-    console.log(3)
-    page_val = 2
-  }
-
-  function getVal() {
-    return page_val
+    set_page_val(2)
   }
 
   function getPage() {
-    console.log('func')
-
-    var p_v = getVal()
-
-    if (p_v===0){
-      return <p>Food scarcity</p>
-    } else if (p_v === 1) {
+    if (page_val===0){
+      return <FoodScarcity/>
+    } else if (page_val === 1) {
       return <p>topic 2</p>
     } else {
       return <p>topic 3</p>
@@ -41,17 +31,16 @@ export function FeedPage() {
     <div>
       <div className="flex d-flex flex-row w-100 text-center justify-content-center">
         <div className="bg-secondary col justify-content-center align-items-center text-dark">
-          <button onClick={foodScarcityBtn} className="p-1 m-2">Food Scaricity</button>
+          <button onClick={foodScarcityBtn} className="w-100 h-100 p-3" style={{boxShadow:'none'}}>Food Scaricity</button>
         </div>
         <div className="bg-primary col justify-content-center align-items-center">
-          <button onClick={secondFocus} className="p-1 m-2">Topic 2</button>
+          <button onClick={secondFocus} className="w-100 h-100 p-3">Topic 2</button>
         </div>
         <div className="bg-success col justify-content-center align-items-center">
-          <button onClick={thirdFocus} className="p-1 m-2">Topic 3</button>
+          <button onClick={thirdFocus} className="w-100 h-100 p-3">Topic 3</button>
         </div>
       </div>
       {getPage()}
-      {console.log('ran here')}
     </div>
   );
 }
---
+++ src/components/FoodScarcity.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export function FoodScarcity() {
+    return (
+        <div className='w-100 bg-primary alig-items-center p-2'>
+            <h1 className='display-1'>Food scarcity</h1>
+        </div>
+    )
+}
