e28f07a995ae7387398ea20d52e56b51ffe9e004
SuragNuthulapaty <77648585+SuragNuthulapaty@users.noreply.github.com>
2023-09-20 22:38:26 -0500

    flesh out food scarcity

--- src/components/FeedPage.js
+++ src/components/FeedPage.js
@@ -1,20 +1,52 @@
 import React from "react";
 import { useState } from "react";
 import { FoodScarcity } from "./FoodScarcity";
+import { Link } from "react-router-dom/cjs/react-router-dom.min";
 
 export function FeedPage() {
   const [page_val, set_page_val] = useState(0);
+  const [fs_color, set_fs_color] = useState('lightgray')
+  const [t2_color, set_t2_color] = useState('white')
+  const [t3_color, set_t3_color] = useState('white')
+  const [fs_bw, sfsbw] = useState('0px')
+  const [t2_bw, st2bw] = useState('2px')
+  const [t3_bw, st3bw] = useState('2px')
+
 
   function foodScarcityBtn() {
     set_page_val(0)
+
+    set_t2_color('white')
+    set_t3_color('white')
+    set_fs_color('lightgray')
+
+    sfsbw('0px')
+    st2bw('2px')
+    st3bw('2px')
   }
 
   function secondFocus() {
     set_page_val(1)
+
+    set_fs_color('white')
+    set_t3_color('white')
+    set_t2_color('lightgray')
+
+    sfsbw('2px')
+    st2bw('0px')
+    st3bw('2px')
   }
 
   function thirdFocus() {
     set_page_val(2)
+
+    set_t2_color('white')
+    set_fs_color('white')
+    set_t3_color('lightgray')
+
+    sfsbw('2px')
+    st2bw('2px')
+    st3bw('0px')
   }
 
   function getPage() {
@@ -31,16 +63,19 @@ export function FeedPage() {
     <div>
       <div className="flex d-flex flex-row w-100 text-center justify-content-center p-2">
         <div className="col justify-content-center align-items-center text-dark m-1 p-2">
-          <button onClick={foodScarcityBtn} className="w-100 h-100 p-3" style={{borderRadius:'20px', backgroundColor:'white'}}>Food Scaricity</button>
+          <button onClick={foodScarcityBtn} className="w-100 h-100 p-3" style={{borderRadius:'20px', backgroundColor:fs_color, borderWidth:fs_bw}}>Food Scaricity</button>
         </div>
         <div className="col justify-content-center align-items-center m-1 p-2">
-          <button onClick={secondFocus} className="w-100 h-100 p-3" style={{borderRadius:'20px', backgroundColor:'white'}}>Topic 2</button>
+          <button onClick={secondFocus} className="w-100 h-100 p-3" style={{borderRadius:'20px', backgroundColor:t2_color, borderWidth:t2_bw}}>Topic 2</button>
         </div>
         <div className="col justify-content-center align-items-center m-1 p-2">
-          <button onClick={thirdFocus} className="w-100 h-100 p-3" style={{borderRadius:'20px', backgroundColor:'white'}}>Topic 3</button>
+          <button onClick={thirdFocus} className="w-100 h-100 p-3" style={{borderRadius:'20px', backgroundColor:t3_color, borderWidth:t3_bw}}>Topic 3</button>
         </div>
       </div>
       {getPage()}
+      <div className="flex d-flex align-items-center justify-content-center pb-3">
+        <Link to={{ pathname:"/"}} className="text-white rounded p-2 bg-dark h-25" style={{textDecoration:'none'}}>Return To Home Page</Link>
+      </div>
     </div>
   );
 }
--- src/components/FoodScarcity.jsx
+++ src/components/FoodScarcity.jsx
@@ -1,19 +1,86 @@
 import React from 'react'
+import junk_price_img from "../images/compare_price_img.jpeg"
+import food_stamps from "../images/food_stamps.png"
+import food_desert from "../images/food_desert.jpeg"
 
 export function FoodScarcity() {
     return (
-        <div className='w-100 bg-primary alig-items-center p-2'>
+        <div className='w-100 alig-items-center p-2'>
             <h1 className='display-2 font-weight-bold'>Food scarcity</h1>
+
+            <hr />
+
+            <div className='flex d-flex p-1'>
+                <div className='col m-2'>
+                    <h2 className='display-5 fs-1'>Facts about food:</h2>
+                    <ul className='text-md fs-5'>
+                        <li className=''>
+                            It is an integral part of our lives, needed for daily function and survival.
+                        </li>
+                        <li className=''>
+                            No matter who we are or what we do, we need food to live.
+                        </li>
+                        <li>
+                            Good food and affordable food are not the same thing in the united states, in fact precisely the opposite. Junk food is very cheap and healthy food is more expensive.
+                        </li>
+                    </ul>
+                </div>
+                <div className='flex d-flex col m-2 align-items-center justify-content-center'>
+                    <img src={junk_price_img} style={{height:'400px',width:'400px'}}/>
+                </div>
+            </div>
+
             <hr/>
-            <h2 className='display-5 fs-1'>How we treat food</h2>
-            <ul className='text-md fs-5'>
-                <li className=''>
-                    akjsdh
-                </li>
-                <li className=''>
-                    askjsdh
-                </li>
-            </ul>
+
+            <div className='flex d-flex p-1'>
+                <div className='col m-2'>
+                    <h2 className='display-5 fs-1'>Problems with food:</h2>
+                    <ul className='text-md fs-5'>
+                        <li className=''>
+                            Food is not treated as a need for everybody, it is being "gatekept".
+                        </li>
+                        <li className=''>
+                            People do not have the desire to contribute to programs that help protect everyones access to food.
+                        </li>
+                        <li>
+                            There is heavy stigma against people who use food aiding products, like food stamps, to gain access to much needed food.
+                        </li>
+                        <li>
+                            Food deserts cheap food, are not being addressed.
+                        </li>
+                    </ul>
+                </div>
+                <div className='flex d-flex col m-2 align-items-center justify-content-center'>
+                    <img src={food_stamps} style={{height:'200px',width:'300px'}}/>
+                </div>
+            </div>
+
+            <hr/>
+
+            <div className='flex d-flex p-1'>
+                <div className='col m-2'>
+                    <h2 className='display-5 fs-1'>Food Deserts:</h2>
+                    <ul className='text-md fs-5'>
+                        <li className=''>
+                            Food deserts are vast areas of space without easy access to cheap, healthy food.
+                        </li>
+                        <li>
+                            In addition, many people in food deserts do not have access to cars or transportation otehr than unreliable public transit.
+                        </li>
+                        <li className=''>
+                            This forces residents in those areas to have to either spend hours going to get food, or settle with the junk food they can get.
+                        </li>
+                        <li>
+                            People who live in food deserts are disproportionally those of marginilized races, and thus disproportionally impacted by them.
+                        </li>
+                    </ul>
+                </div>
+                <div className='flex d-flex col m-2 align-items-center justify-content-center'>
+                    <img src={food_desert} style={{height:'200px',width:'300px'}}/>
+                </div>
+            </div>
+
+            <hr/>
 
         </div>
     )
